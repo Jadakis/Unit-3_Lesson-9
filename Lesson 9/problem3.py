cf2a6e4c1d9846bbe67e526ce56df27df621f499
Jadakis <44173632+Jadakis@users.noreply.github.com>
2018-12-17 12:48:35 -0500

    Add files via upload

---
+++ Lesson 9/problem3.py
@@ -0,0 +1,10 @@
+print('-'*60)
+print('Alien Greeter Bot:')
+print()
+password = input('What planet are you from? ')
+if password == 'Earth':
+	print('Dammmmmnnnnn, your face is so ugly I thought you were an alien. Anyway . . .have a good day!')
+else:
+	print('Welcome to Earth, please do not kill us!')
+	
+print('-'*60)
